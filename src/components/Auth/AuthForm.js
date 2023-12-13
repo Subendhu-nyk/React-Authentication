9cb5655cebc1eb8b2457cc81243e6fe3d4b9144a
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-12-13 11:12:03 +0530

    Implementing Login (t-16)

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -19,10 +19,14 @@ const AuthForm = () => {
     const enteredPassword=passwordInputRef.current.value
 
     setIsLoading(true)
+    let url
     if(isLogin){
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAhsI_8C_gFMLKp6R0Tf0-INRYsTS-lndg'
 
     }else{
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAhsI_8C_gFMLKp6R0Tf0-INRYsTS-lndg',
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAhsI_8C_gFMLKp6R0Tf0-INRYsTS-lndg'
+    }
+    fetch(url,
       {
         method:'POST',
         body:JSON.stringify({
@@ -37,19 +41,24 @@ const AuthForm = () => {
       ).then((res)=>{
         setIsLoading(false)
         if(res.ok){
-
+          return res.json();
         }else{
          return res.json().then(data=>{
           let errorMessage ='Authentication Failed!';
           if(data && data.error && data.error.message){
             errorMessage=data.error.message  
           } 
-          alert(errorMessage)                   
+          alert(errorMessage)     
+          throw new Error(errorMessage);              
           })
         }
+      }).then(data=>{
+        console.log(data)
+        localStorage.setItem('ReactToken',data.idToken)
       })
-
-    }
+      .catch((err)=>{
+        alert(err.message)
+      })
 
   }
 
